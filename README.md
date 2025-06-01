eeddc810497aa882039f3c2888f0ec9241159c3a
mu-kumaran <mu.kumaran1706@gmail.com>
2025-06-01 18:59:30 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Library Management App
+-----------------------
+In this project, we will create a Library Management Application which will have the following functionalities,
+- Fetch all the available books in the library
+- Add new books
+- Lend books
+- Keep a track of borrowed books
+- Update the details based on the books returned 
+- Remove books from database
+
+Project flow
+------------
+- In this project 'LibraryDatabase.txt' file has been used as the books collection database. This filename should be entered at the start of the applilcation for loading database for this project.
+- The very first step from the user's point of view would be to display the menu of the library management application.
+- Following would be the main menu options
+1. Display Books
+2. Lend a Book
+3. Add a Book
+4. Return a Book
+5. Display Books borrowed
+6. Remove a Book
+7. Exit
+
+- Depending on the choice entered, we would need to perform the operation and return the details to the output screen.
+- The above process has to be done repeatedly until the user quits.
+
+if __name__ == '__main__': is the entry point of the interpreter instead of top down apporach
---
+++ projectFile.py
@@ -0,0 +1,124 @@
+# Library management app
+
+class Library:
+    def __init__(self,bookslist):
+        self.bookslist = bookslist
+        self.booksBorrowed = {}
+    
+    def displayBooks(self):
+        print("Books in the library:")
+        print("--------------------")
+        for i in self.bookslist:
+            print(i)
+
+    def displayBooksBorrowed(self):
+        print("Books Borrowed from library:")
+        print("---------------------------")
+        if(not self.booksBorrowed):
+            print("No books have been borrowed yet")
+        else:
+            for (i,j) in self.booksBorrowed.items():
+                print(i,": lended by ",j)
+
+    def addBooks(self):
+        book = input("Enter the book name to add in the library: ")
+        self.bookslist.append(book)
+        booksDB = open(booksDBase,'a')
+        booksDB.write("\n")
+        booksDB.write(book)
+        print("Book added")
+    
+    def removeBooks(self):
+        book = input("Enter the book name to remove in the library: ")
+        if book in bookslist:
+            self.bookslist.remove(book)
+            booksDB = open(booksDBase,'w')
+            length = len(self.bookslist)
+            j=1
+            for i in self.bookslist:
+                if j == length:
+                    booksDB.write(i)
+                else:
+                    booksDB.write(i)
+                    booksDB.write("\n")
+                    j += 1
+            print("Book removed")
+        else:
+            print("No such book available in the library")
+
+    def lendBooks(self):
+        book = input("Enter the book name to lend from the library: ")
+        if book not in self.bookslist:
+            print("No such book available. Enter the correct book name")
+        else:
+            user = input("Enter the name of the user: ")
+
+            if book in self.booksBorrowed.keys():
+                print(f"Book already lended by {self.booksBorrowed.get(book)}")
+            else:
+                self.booksBorrowed.update({book:user})
+                print("Book lended")
+
+    def returnBooks(self):
+        book = input("Enter the book name to return to the library: ")
+        user = input("Enter the user who have lended the book:")
+        if (book,user) in self.booksBorrowed.items():
+            del self.booksBorrowed[book]
+            print("Book returned")
+        else:
+            print("Book cannot be returned")
+
+def main():
+    print("Welcome to the Library: ")
+    print("----------------------")
+    while(True):
+        print("""
+                Options:
+                -------- 
+                1. Display books in the library
+                2. Display books borrowed from library
+                3. Add Books in the Library
+                4. Remove Books in the Library
+                5. Lend Book
+                6. Return Book
+                7. Exit
+        """)
+        try:
+            option = int(input("Enter the option number:"))
+            if(option == 1):
+                lib.displayBooks()
+            elif(option == 2):
+                lib.displayBooksBorrowed()
+            elif(option == 3):
+                lib.addBooks()
+            elif(option == 4):
+                lib.removeBooks()
+            elif(option == 5):
+                lib.lendBooks()
+            elif(option == 6):
+                lib.returnBooks()
+            elif(option == 7):
+                break
+            else:
+                print("Wrong option. Try again")
+        except Exception as err:
+            print("Typing Error:",err)
+            print("Enter correct option")
+            
+
+
+if __name__ == "__main__":
+    while(True):
+        booksDBase = input("Enter the books database filename: ")
+        try:
+            booksDB = open(booksDBase,'r')
+            books = booksDB.read()      # returns a string datatype
+            bookslist = books.split("\n")   # string datatype converted in to list
+            lib = Library(bookslist)
+            break
+        except Exception as err:
+            print("Error: ",err)
+            print("Enter the correct filename")
+
+    main()
+
